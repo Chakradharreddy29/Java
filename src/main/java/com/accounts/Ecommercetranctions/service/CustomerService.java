f9b275d3f44d57438b19afcb9c068e35997fb951
Goli Vishnuvardhan Reddy <vishnuvardhan.goli315@gmail.com>
2024-03-03 19:46:18 +0530

    commit

--- src/main/java/com/accounts/Ecommercetranctions/service/CustomerService.java
+++ src/main/java/com/accounts/Ecommercetranctions/service/CustomerService.java
@@ -1,7 +1,7 @@
 package com.accounts.Ecommercetranctions.service;
 
 import com.accounts.Ecommercetranctions.dto.CustometDto;
-
+//main class
 public interface CustomerService {
 	CustometDto createCustomer (CustometDto custometDto); 
 
